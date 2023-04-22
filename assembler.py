c2aef1c8a73338cb35d53c8ee44bbf004c0f4c7e
sahilguptasg2017 <117901246+sahilguptasg2017@users.noreply.github.com>
2023-04-23 03:07:52 +0530

    update

---
+++ assembler.py
@@ -0,0 +1,11 @@
+operations={"Add":'00000','Sub':'00001','mov_imm':'00010','mov_reg':'00011',
+            'load':'00100','store':'00101','mul':'00110','div':'00111',
+            'right_shift':'01000','left_shift':'01001','excl_or':'01010',
+            'or':'01011','and':'01100','invert':'01101','compare':'01110',
+            'uncond_jump':'01111','jump_if_less':'11100','jum_if_greater':'11101',
+            'jum_if_equal':'11111','halt':'11010'}
+
+registers={'R0':'000','R1':'001',
+           'R2':'010','R3':'011',
+           'R4':'100','R5':'101',
+           'R6':'110','FLAGS':'111'}
