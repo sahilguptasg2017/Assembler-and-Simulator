f6ca34badda9a9b3ae5e41908c63af5303d60fc5
tanmay22535 <119058077+tanmay22535@users.noreply.github.com>
2023-05-01 23:43:52 +0530

    fixed things and improved readability

--- assembler.py
+++ assembler.py
@@ -28,27 +28,27 @@ registers={'R0':'000',
            'R6':'110',
            'FLAGS':'111'}
 
-x=open(r"stdin.txt","r+")
-file_1=open(r"stdout.txt","w")
-y=x.readlines()
+inp_file=open(r"stdin.txt","r+")
+out_file=open(r"stdout.txt","w")
+inp_lines=inp_file.readlines()
 #file_1.write(y)
-count=0
+var_count=0
 
-for n in y:
-    if 'var' in n:
-        count+=1
+for line in inp_lines:
+    if 'var' in line:
+        var_count+=1
 #file_1.write(count)        
         
-count1=-count
+count1=-var_count
 
-for u in y:
-    if u!='\n':
+for line in inp_lines:
+    if line!='\n':
         count1+=1
 #file_1.write(count1) 
 
 d={}
 
-for r in y:
+for r in inp_lines:
     if 'var' in r:
         r=r.replace(" ","")
         #file_1.write(r)
@@ -56,7 +56,7 @@ for r in y:
         count1+=1        
 #file_1.write(d)
 
-for i in y:
+for i in inp_lines:
     s=""
     if i!='\n':
         z1=i.split(" ")
@@ -80,30 +80,30 @@ for i in y:
                         s+=registers[z[1]]
                         s+='0'*(7-len(bin(int(z[2][1:]))[2:]))
                         s+=bin(int(z[2][1:]))[2:]    
-                        file_1.write(f'{s}\n')
+                        out_file.write(f'{s}\n')
                     else:
                         s+=operations['mov1']
                         s+='0'*5
                         s+=registers[z[1]]
                         s+=registers[z[2]]
-                        file_1.write(f'{s}\n')
+                        out_file.write(f'{s}\n')
                 elif z[0]=='mul':
                     s+=operations['mul']
                     s+='0'*2
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='st':
                     s+=operations['st']    
                     s+='0'
                     s+=registers[z[1]]
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='hlt':
                     s+=operations['hlt']
                     s+=11*'0'
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                     break
                 elif z[0]=='add':
                     s+=operations['add']
@@ -111,91 +111,91 @@ for i in y:
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='sub':
                     s+=operations['sub']
                     s+='0'*2
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='ld':
                     s+=operations['ld']
                     s+='0'*1
                     s+=registers[z[1]]
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='div':
                     s+=operations['div']
                     s+='0'*5
                     s+=registers[z[1]]
                     s+=registers[z[2]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='rs':
                     s+=operations['rs']
                     s+='0'*1
                     s+=registers[z[1]]
                     s+=bin(int(z[2][1:]))[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='ls':
                     s+=operations['ls']
                     s+='0'*1
                     s+=registers[z[1]]
                     s+=bin(int(z[2][1:]))[2:]    
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='xor':
                     s+=operations['xor']
                     s+='0'*2
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='or':
                     s+=operations['or']    
                     s+='0'*2
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='and':
                     s+=operations['and']
                     s+='0'*2
                     s+=registers[z[1]]
                     s+=registers[z[2]]
                     s+=registers[z[3]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='not':
                     s+=operations['not']
                     s+='0'*5
                     s+=registers[z[1]]
                     s+=registers[z[2]]  
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='cmp':
                     s+=operations['cmp']
                     s+='0'*5
                     s+=registers[z[1]]
                     s+=registers[z[2]]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='jmp':
                     s+=operations['cmp']
                     s+='0'*4
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='jlt':
                     s+=operations['jlt']
                     s+='0'*4
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                 elif z[0]=='jgt':
                     s+=operations['jgt']
                     s+='0'*4
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')        
+                    out_file.write(f'{s}\n')        
                 elif z[0]=='je':
                     s+=operations['je']
                     s+='0'*4
                     s+='0'*(7-len(bin(d[z[2]])[2:]))+bin(d[z[2]])[2:]
-                    file_1.write(f'{s}\n')
+                    out_file.write(f'{s}\n')
                     
 
 
