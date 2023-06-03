d4d12138626325c9ecf1d358ef9f1cb4fbb02f8e
sahilguptasg2017 <117901246+sahilguptasg2017@users.noreply.github.com>
2023-06-03 18:00:44 +0530

    small update

--- simulator.py
+++ simulator.py
@@ -1,11 +1,22 @@
 import sys
+
+
+program_counter=0
+
+
+
+def program_counter_update(program_counter):
+    program_counter+=1
+    binary=bin(program_counter)
+    return program_counter
+
+
 def input_read():
     inp_lines=sys.stdin.readlines()
     return inp_lines
-
 
 def file_write(out_lst):
     for line in out_lst:
         print(line)
 
-        
+
