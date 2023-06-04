e06cde5b12ff922ac1ad459b2a3f4a9be3889d55
Rachit Arora <71589920+rcht@users.noreply.github.com>
2023-06-04 23:20:22 +0530

    simulator almost done

--- simulator.py
+++ simulator.py
@@ -1,44 +1,71 @@
 import sys
-opCodeOf={"add":'00000',
-            'sub':'00001',
-            'mov':'00010',
-            'mov1':'00011',
-            'ld':'00100',
-            'st':'00101',
-            'mul':'00110',
-            'div':'00111',
-            'rs':'01000',
-            'ls':'01001',
-            'xor':'01010',
-            'or':'01011',
-            'and':'01100',
-            'not':'01101',
-            'cmp':'01110',
-            'jmp':'01111',
-            'jlt':'11100',
-            'jgt':'11101',
-            'je':'11111',
-            'hlt':'11010'}
+import operator
 
-# Dictionary for register addresses in binary
-registers={'R0':'000',
-           'R1':'001',
-           'R2':'010',
-           'R3':'011',
-           'R4':'100',
-           'R5':'101',
-           'R6':'110',
-           'FLAGS':'111'}
-# Dictionary for register addresses in binary
-registers_value={'R0': 0,
-           'R1':0,
-           'R2':0,
-           'R3':0,
-           'R4':0,
-           'R5':0,
-           'R6':0,
-           'FLAGS':0
-           }
+def binToInt(s):
+    exp = 1
+    ans = 0
+    while s:
+        ans += (exp if s[-1] == '1' else 0)
+        exp *= 2
+        s = s[:-1]
+    return ans
+
+def intToBin(n):
+    s = ""
+    for i in range(16):
+        s = chr(ord('0') + n%2) + s
+        n //= 2
+    return s
+
+def intToPC(n):
+    s = ""
+    for i in range(7):
+        s = chr(ord('0') + n%2) + s
+        n //= 2
+    return s + ' '*8 # 8 spaces required by output for some reason
+
+insCodeOf={'00000': "add",
+           '00001':'sub',
+           '00010':'mov',
+           '00011':'mov1',
+           '00100':'ld',
+           '00101':'st',
+           '00110':'mul',
+           '00111':'div',
+           '01000':'rs',
+           '01001':'ls',
+           '01010':'xor',
+           '01011':'or',
+           '01100':'and',
+           '01101':'not',
+           '01110':'cmp',
+           '01111':'jmp',
+           '11100':'jlt',
+           '11101':'jgt',
+           '11111':'je',
+           '11010':'hlt'}
+
+# List for register addresses in binary
+registers = [0]*8
+memory = [0]*128
+FLAGS = 7
+
+PC = 0
+executing = True
+
+def next():
+    global PC
+    PC += 1
+
+def getBinary():
+    inp = sys.stdin.readlines()
+    finale = [i.strip() for i in inp]
+    return finale
+
+def dumpState(oldPC):
+    print(intToPC(oldPC), end = "")
+    print(*[intToBin(i) for i in registers])
+
 # Counting number of vars
 reg3ins = ["add", "sub", "mul", "xor", "or", "and"] # type A
 immins = ["mov", "rs", "ls"] # type B
@@ -46,105 +73,180 @@ reg2ins = ["mov1", "div", "not", "cmp"] # type C
 memins = ["ld", "st"] # type D
 jmpins = ["jmp", "jlt", "jgt", "je", "hlt"] # type E   
 
-program_counter=0
+operatorOf = {
+    "add": operator.add,
+    "sub": operator.sub,
+    "mul": operator.mul,
+    "or": operator.__or__,
+    "and": operator.__and__,
+    "xor": operator.xor
+}
 
-def increase_programcounter():
-    global program_counter
-    program_counter+=1
+print(operatorOf["xor"](4,4))
 
-def input_read():
-    inp_lines=sys.stdin.readlines()
-    return inp_lines
+def validImmediate(reg): 
+    return (reg >= 0) and (reg < 128)
 
-def file_write(out_lst):
-    for line in out_lst:
-        print(line)
+def typeA(ins, reg1, reg2, reg3):
+    global registers
 
-def mov(address1 , address2 , address3):
-    increase_programcounter()
-    reg1 = ""
-    reg2 = ""
-    reg3 = ""
-    for r , v in registers.items():
-        if(v == address1):
-            reg1 = r
-        if(v == address2):
-            reg2 = r
-        if(v == address3):
-            reg3 = r
-    registers_value[reg1] = registers_value[reg2] - registers_value[reg3]
-def mov1(address1 , immediate):
-    increase_programcounter()
-    reg1 = ""
-    for r , v in registers.items():
-        if(v == address1):
-            reg1 = r
-            break
-    registers_value[reg1] = immediate
-def cmp(address1 ,address2):
-    increase_programcounter()
-    reg1 = ""
-    reg2 = ""
-    for r , v in registers.items():
-        if(v == address1):
-            reg1 = r
-        if(v == address2):
-            reg2 = r
-    val1 = registers_value[reg1]
-    val2 = registers_value[reg2]
-    if( val1 == val2 ):
-        registers_value['FLAGS'] = 1
-    elif( val1 > val2 ):
-        registers_value['FLAGS'] = 2
+    registers[reg1] = operatorOf[ins](registers[reg2], registers[reg3]) 
+    if not validImmediate(registers[reg1]):
+        registers[reg1] = 0
+        registers[FLAGS] |= 8 # forcefully set overflow flag 
     else:
-        registers_value['FLAGS'] = 4        
+        registers[FLAGS] &= 119 # 127 - 8 
 
-def appending_output(out_lst):
-    string="0"(7-len(bin(program_counter)[2:]))+bin(program_counter)[2:]+" "*8
-    for i in registers_value:
-        string+=(16-len(bin(registers_value[i][2:])))*0+bin(registers_value[i])[2:]+" " 
-    out_lst.appemd(string)        
+    next()
 
-def hlt(out_lst):
-    global program_counter
-    increase_programcounter()
-    appending_output(out_lst)
-    file_write()    
+def typeB(ins, reg1, im1):
+    global registers
+    if ins=='mov':
+        registers[reg1] = im1
+    elif ins=='rs':
+        registers[reg1] >>= im1
+    else:
+        registers[reg1] <<= im1
+        registers[reg1] %= 128
 
-def binarytodecimal(line):
-    line=line[::-1]
-    ans=0
-    for i in range(len(line)):
-        ans+=(2**i)*line[i]
-    return ans    
+    next()
 
-def jmp(out_lst,line):
-    program_counter=binarytodecimal(line[9:])
-    
-
-
-inp_lines=input_read()
-
-def main():
-    out_lst=[]
-    for i in range(len(inp_lines)):
-        line = inp_lines[i]
-        opcode = line[0:5]
-        if(opCodeOf['hlt']==opcode):
-            hlt()
-            break
-        
-
-
-
-
-    
-
-
-
-
-if __name__=="__main__":
-    main()
+def typeC(ins, reg1, reg2):
+    global registers
+    if ins=='mov1':
+        registers[reg1] = registers[reg2]
+    elif ins=='div':
+        if registers[reg2] == 0:
+            registers[reg1] = 0
+            registers[FLAGS] |= 8
+        else:
+            registers[reg1] //= registers[reg2]
+    elif ins=='not':
+        registers[reg1] = 127 - registers[reg2]
+    else:
+        # cmp
+        if registers[reg1] < registers[reg2]:
+            registers[FLAGS] |= 4
+            registers[FLAGS] &= 125 # 127 - 2
+            registers[FLAGS] &= 126 # 127 - 1
+        elif registers[reg1] == registers[reg2]:
+            registers[FLAGS] &= 123 # 127 - 4
+            registers[FLAGS] &= 125 # 127 - 2
+            registers[FLAGS] |= 1
+        else:
+            registers[FLAGS] &= 123
+            registers[FLAGS] |= 2
+            registers[FLAGS] &= 126
+    next()
 
 
+def typeD(ins, reg1, mem1):
+    global registers
+    if ins=='ld':
+        registers[reg1] = memory[mem1]
+    else:
+        memory[mem1] = registers[reg1]
+    next()
 
+# MAIN
+
+binary = getBinary()
+print(binary)
+
+for i in range(len(binary)):
+    memory[i] = binToInt(binary[i])
+
+while executing:
+    old = PC
+    current = binary[PC]
+    opc = current[:5]
+    ins = insCodeOf[opc]
+
+    # type A
+    if ins in reg3ins:
+        r1 = binToInt(current[7:10])
+        r2 = binToInt(current[10:13])
+        r3 = binToInt(current[13:])
+        registers[r1] = operatorOf[ins](registers[r2], registers[r3]) 
+        if not validImmediate(registers[r1]):
+            registers[r1] = 0
+            registers[FLAGS] |= 8 # forcefully set overflow flag 
+        else:
+            registers[FLAGS] &= 119 # 127 - 8 
+        next()
+
+    # type B
+    elif ins in immins:
+        reg1 = binToInt(current[6:9]) # nice ;)
+        im1 = binToInt(current[9:])
+        if ins=='mov':
+            registers[reg1] = im1
+        elif ins=='rs':
+            registers[reg1] >>= im1
+        else:
+            registers[reg1] <<= im1
+            registers[reg1] %= 128
+
+        next()
+
+    # type C
+    elif ins in reg2ins:
+        reg1 = binToInt(current[10:13])
+        reg2 = binToInt(current[13:])
+        if ins=='mov1':
+            registers[reg1] = registers[reg2]
+        elif ins=='div':
+            if registers[reg2] == 0:
+                registers[reg1] = 0
+                registers[FLAGS] |= 8
+            else:
+                registers[reg1] //= registers[reg2]
+                registers[FLAGS] &= 119
+        elif ins=='not':
+            registers[reg1] = 127 - registers[reg2]
+        else:
+            # cmp
+            if registers[reg1] < registers[reg2]:
+                registers[FLAGS] |= 4
+                registers[FLAGS] &= 125 # 127 - 2
+                registers[FLAGS] &= 126 # 127 - 1
+            elif registers[reg1] == registers[reg2]:
+                registers[FLAGS] &= 123 # 127 - 4
+                registers[FLAGS] &= 125 # 127 - 2
+                registers[FLAGS] |= 1
+            else:
+                registers[FLAGS] &= 123
+                registers[FLAGS] |= 2
+                registers[FLAGS] &= 126
+        next()
+
+    # type D
+    elif ins in memins:
+        reg1 = binToInt(current[6:9])
+        mem1 = binToInt(current[9:])
+        if ins=='ld':
+            registers[reg1] = memory[mem1]
+        else:
+            memory[mem1] = registers[reg1]
+        next()
+
+    # type E
+    else:
+        mem1 = current[9:]
+        if ins=='jmp':
+            PC = mem1
+        elif ins=='jlt':
+            if registers[FLAGS] & 4:
+                PC = mem1
+        elif ins=='jgt':
+            if registers[FLAGS] & 2:
+                PC = mem1
+        elif ins=='je':
+            if registers[FLAGS] & 1:
+                PC = mem1
+        else:
+            executing = False
+        next()
+
+
+    dumpState(old)
